f4a454e59f355d1c216b8bc7b09c630b1727546d
Jane <janeluciaschoenfeld@gmail.com>
2022-05-13 15:06:25 +0200

    active link in navbar turns white

--- src/frontend/component/Navi.jsx
+++ src/frontend/component/Navi.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Home } from '../styles/images/navi-svg/home.svg';
 import { ReactComponent as Dumbbell } from '../styles/images/navi-svg/dumbbell.svg';
 import { ReactComponent as Profile } from '../styles/images/navi-svg/profil.svg';
@@ -6,15 +6,24 @@ import { ReactComponent as Profile } from '../styles/images/navi-svg/profil.svg'
 const Navi = () => {
   return (
     <nav className="w-full rounded-tl-3xl rounded-tr-3xl flex justify-around gap-x-12 fixed bottom-0 bg-gradient-to-r from-dark px-5 py-2 text-medium">
-      <Link to="/" className="active:text-light">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? 'text-light' : '')}
+      >
         <Home />
-      </Link>
-      <Link to="/browse">
+      </NavLink>
+      <NavLink
+        to="/browse"
+        className={({ isActive }) => (isActive ? 'text-light' : '')}
+      >
         <Dumbbell />
-      </Link>
-      <Link to="/profile">
+      </NavLink>
+      <NavLink
+        to="/profile"
+        className={({ isActive }) => (isActive ? 'text-light' : '')}
+      >
         <Profile />
-      </Link>
+      </NavLink>
     </nav>
   );
 };
