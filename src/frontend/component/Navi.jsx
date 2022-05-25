ec1aa9628ba1986bd3bd881081d278aaaba494de
Jane <janeluciaschoenfeld@gmail.com>
2022-05-25 15:58:56 +0200

    nav link seperate component

--- src/frontend/component/Navi.jsx
+++ src/frontend/component/Navi.jsx
@@ -3,15 +3,23 @@ import { ReactComponent as Home } from '../styles/images/navi-svg/home.svg';
 import { ReactComponent as Dumbbell } from '../styles/images/navi-svg/dumbbell.svg';
 import { ReactComponent as Profile } from '../styles/images/navi-svg/profil.svg';
 
+const MeinNavLink = ({ children, ...props }) => {
+  return (
+    <NavLink
+      className={({ isActive }) => (isActive ? 'text-light' : '')}
+      {...props}
+    >
+      {children}
+    </NavLink>
+  );
+};
+
 const Navi = () => {
   return (
     <nav className="w-full rounded-tl-3xl rounded-tr-3xl flex justify-around gap-x-12 fixed bottom-0 bg-dark bg-opacity-70 px-5 py-2 text-medium text-opacity-100">
-      <NavLink
-        to="/"
-        className={({ isActive }) => (isActive ? 'text-light' : '')}
-      >
+      <MeinNavLink to="/">
         <Home />
-      </NavLink>
+      </MeinNavLink>
       <NavLink
         to="/browse"
         className={({ isActive }) => (isActive ? 'text-light' : '')}
