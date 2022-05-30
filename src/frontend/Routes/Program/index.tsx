9f302babe775504916573a8f55b7b7f74c9fbf5f
Jane <janeluciaschoenfeld@gmail.com>
2022-05-30 13:58:41 +0200

    program workout lacout

--- src/frontend/Routes/Program/index.tsx
+++ src/frontend/Routes/Program/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import H1 from '../../component/font/H1';
 import { gql, useQuery } from '@apollo/client';
-import ST from '../../component/font/SmallText';
+import H1 from '../../component/font/H1';
+import H3 from '../../component/font/H3';
 import P from '../../component/font/P';
-import Circle from '../../component/Circle'
+import ST from '../../component/font/SmallText';
+import Circle from '../../component/Circle';
+import Card from '../../component/Card';
 
 type ProgramProps = {};
 
@@ -12,7 +14,7 @@ const Program = () => {
   const { id } = useParams();
   const PROGRAM_QUERY = gql`
     query GetProgram($id: ID!) {
-      program(where: {id: $id}) {
+      program(where: { id: $id }) {
         description
         difficulty
         focus
@@ -25,42 +27,69 @@ const Program = () => {
         workouts {
           id
           name
+          duration
+          category
         }
       }
     }
   `;
-  const { loading, error, data } = useQuery(PROGRAM_QUERY, {variables: {id}});
+  const { loading, error, data } = useQuery(PROGRAM_QUERY, {
+    variables: { id },
+  });
   if (loading) return <p className="text-light text-center">loading ...</p>;
   if (error) return <p className="text-light text-center">error :/ </p>;
   const backgroundImage = {
     backgroundImage: `url(${data.program.image.url})`,
     height: '70vh',
-    backgroundSize: 'cover'
-  }
+    backgroundSize: 'cover',
+  };
+  const workoutOverview = data.program.workouts.map((workout) => {
+    return (
+      <Card className="">
+        <H3>{workout.name}</H3>
+        <ST>
+          {workout.duration} · {workout.category}
+        </ST>
+      </Card>
+    );
+  });
   return (
     <>
       <header>
-        <section className="flex flex-col justify-end items-center px-5 text-center" style={backgroundImage}>
+        <section
+          className="flex flex-col justify-end items-center px-5 text-center"
+          style={backgroundImage}
+        >
           <H1>{data.program.name}</H1>
           <div className="flex justify-around w-full items-end pb-5 h-1/2">
-            <div className='flex flex-col items-center gap-y-2'>
-              <Circle className='w-8 h-8 bg-gradient-to-br from-gradient1A to-gradient1B'/>
+            <div className="flex flex-col items-center gap-y-2">
+              <Circle className="w-8 h-8 bg-gradient-to-br from-gradient1A to-gradient1B" />
               <ST>{data.program.focus}</ST>
             </div>
-            <div className='flex flex-col items-center gap-y-2'>
-              <Circle className='w-8 h-8 bg-gradient-to-br from-gradient2A to-gradient2B'/>
+            <div className="flex flex-col items-center gap-y-2">
+              <Circle className="w-8 h-8 bg-gradient-to-br from-gradient2A to-gradient2B" />
               <ST>{data.program.difficulty}</ST>
             </div>
-            <div className='flex flex-col items-center gap-y-2'>
-              <Circle className='w-8 h-8 bg-gradient-to-br from-gradient3A via-gradient3B to-gradient3C'/>
+            <div className="flex flex-col items-center gap-y-2">
+              <Circle className="w-8 h-8 bg-gradient-to-br from-gradient3A via-gradient3B to-gradient3C" />
               <ST>{data.program.duration} weeks</ST>
             </div>
           </div>
         </section>
       </header>
       <main>
-        <section className='px-5 py-5 bg-medium'>
+        <section className="px-5 py-5 bg-medium">
           <P>{data.program.description}</P>
+        </section>
+        <section className="px-5 py-5">
+          <H3>So ist das Programm aufgeteilt:</H3>
+        </section>
+        <section className="px-5 py-5">
+          <div className="flex justify-between items-center">
+            <H3>{data.program.duration} Weeks</H3>
+            <ST>Alle anzeigen</ST>
+          </div>
+          <div className=" flex flex-col gap-y-2">{workoutOverview}</div>
         </section>
       </main>
     </>
